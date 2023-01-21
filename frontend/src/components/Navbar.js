58fa9daeac3ae5b3df9fc512c23e04cafe3f1128
sumanth cs <sumanthshetty698@gmail.com>
2023-01-21 14:10:59 +0530

    responsive update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,12 @@
-import React,{useContext}from 'react'
+import React, { useContext } from 'react'
 import { Link, useNavigate } from "react-router-dom"
 import logo from "../assests/instagram_logo.svg";
 import "../css/Navbar.css"
 import { LoginContext } from '../context/LoginContext';
 
 
-const Navbar = ({login}) => {
-    const {setModalOpen} = useContext(LoginContext);
+const Navbar = ({ login }) => {
+    const { setModalOpen } = useContext(LoginContext);
     const navigate = useNavigate();
 
     const loginStatus = () => {
@@ -25,7 +25,67 @@ const Navbar = ({login}) => {
                         <li>profile</li>
                     </Link>
                     <Link to={""}>
-                        <button className="primaryBtn" onClick={()=>{setModalOpen(true)}}>logout</button>
+                        <button className="primaryBtn" onClick={() => { setModalOpen(true) }}>logout</button>
+                    </Link>
+
+                </>
+            ]
+        }
+        else {
+            return [
+                <>
+                    <Link to="/login">
+                        <li>Login</li>
+                    </Link>
+
+                    <Link to="/signup">
+                        <li>Signup</li>
+                    </Link>
+                </>
+            ]
+        }
+    }
+
+    const loginStatusMobile = () => {
+        const token = localStorage.getItem("jwt");
+
+        if (login || token) {
+            return [
+                <>
+                    <Link to="/">
+                        <li>
+                            <span class="material-symbols-outlined">
+                                home
+                            </span>
+                        </li>
+                    </Link>
+                    <Link to="/followingpost">
+                        <li>
+                            <span class="material-symbols-outlined">
+                                explore
+                            </span>
+                        </li>
+                    </Link>
+                    <Link to="/createPost">
+                        <li>
+                            <span class="material-symbols-outlined">
+                                add_circle
+                            </span>
+                        </li>
+                    </Link>
+                    <Link to="/profile">
+                        <li>
+                            <span class="material-symbols-outlined">
+                                account_circle
+                            </span>
+                        </li>
+                    </Link>
+                    <Link to={""}>
+                        <li onClick={() => { setModalOpen(true) }}>
+                            <span class="material-symbols-outlined">
+                                logout
+                            </span>
+                        </li>
                     </Link>
 
                 </>
@@ -48,9 +108,12 @@ const Navbar = ({login}) => {
 
     return (
         <div className='navbar'>
-            <img src={logo} alt="logo" onClick={() =>{navigate("/")}}/>
+            <img src={logo} alt="logo" id='insta_logo' onClick={() => { navigate("/") }} />
             <ul className="nav_menu">
                 {loginStatus()}
+            </ul>
+            <ul className="nav_mobile">
+                {loginStatusMobile()}
             </ul>
         </div>
     )
